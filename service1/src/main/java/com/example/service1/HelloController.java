8ecfccee9dcbd1a2ffda0e4e6ca5d03db2d10a6c
Damian Kowalik <da.kowalik666@gmail.com>
2022-08-18 19:02:09 +0200

    first commit

---
+++ service1/src/main/java/com/example/service1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.service1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String getHello() {
+        return "hello";
+    }
+}
